f6b8e48deb305684546066846771b0a1a22e2a74
Brandon Johnson <brandon.johnson@moov.io>
2025-02-07 12:48:28 -0700

    v0.6.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "MoovKit",
-      url: "https://github.com/moovfinancial/moov-ios/releases/download/v0.6.0/MoovKit.xcframework.zip",
-      checksum: "5705885550aa6f785565a7d220eea0704513179826c511db22244412cc73c9fb"
+      url: "https://github.com/moovfinancial/moov-ios/releases/download/v0.6.1/MoovKit.xcframework.zip",
+      checksum: "895626215f91be1ace9542fbcad4f5e03ca9257c581d7c6a6e55a89d18d8c956"
     )
   ]
 )
