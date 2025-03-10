42c963796993f16cd35ca8bf9a15532da58ec5dc
Adam Kornafeld <adam@kornafeld.com>
2025-03-10 19:24:48 -0400

    v0.11.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "MoovKit",
-      url: "https://github.com/moovfinancial/moov-ios/releases/download/v0.6.2/MoovKit.xcframework.zip",
-      checksum: "e35bb135a5b442f2e0e3030823ec4364d841d81d6d14f367925a735652a195c8"
+      url: "https://github.com/moovfinancial/moov-ios/releases/download/v0.11.0/MoovKit.xcframework.zip",
+      checksum: "7eaa5994869fbf0fe9c22213ee63528837f9909a90d03a7ed8a4c196bf106a28"
     )
   ]
 )
