44c9fcd1010e867b454667023eb24c04cc035a87
Adam Kornafeld <adam@kornafeld.com>
2025-04-02 12:43:17 -0400

    checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     .binaryTarget(
       name: "MoovKit",
       url: "https://github.com/moovfinancial/moov-ios/releases/download/v0.12.0/MoovKit.xcframework.zip",
-      checksum: "96cd53ee73b4c3bd81083c04375c12eb5bdfc72bd7cd1bad91dcc0994d0faf0b"
+      checksum: "8032ff7e35af4d04aed7fadd7795966168e95531838d803fcea24440607f5469"
     )
   ]
 )
